14d6b69b95a2c30ce1edc2744f4d6ba3cfda54e8
mamboer <vip_moon@126.com>
2013-04-09 16:38:45 +0800

    let J.evalTpl support the old-fashion template %xx%

--- src/j.evalTpl.js
+++ src/j.evalTpl.js
@@ -10,10 +10,11 @@
 *	//提示信息为："<a href=/u/1>levin</a>"
 * @param {string} str html模板，字段用%包含
 * @param {Object} data json数据
+* @param {Boolean} alternative old-fashion template style like %xxx%
 */
-J.evalTpl = function (str, data) {
+J.evalTpl = function (str, data,alternative) {
 	var result;
-	var patt = new RegExp("@{([a-zA-z0-9]+)}");
+	var patt = new RegExp(alternative?"%([a-zA-z0-9]+)%":"@{([a-zA-z0-9]+)}");
 	while ((result = patt.exec(str)) != null) {
 		var v = data[result[1]] || '';
 		str = str.replace(new RegExp(result[0], "g"), v);
