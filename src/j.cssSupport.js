f84f9d8eab292358955068e71112728bebb6cc9a
mamboer <mamboer@live.com>
2013-07-23 21:15:55 +0800

    fix j.cssSupport

--- src/j.cssSupport.js
+++ src/j.cssSupport.js
@@ -7,24 +7,25 @@
 * @returns {Boolean}
 */
 J.cssSupport = (function() {
-   var div = document.createElement('div'),
-      vendors = 'Khtml Ms O Moz Webkit'.split(' '),
-      len = vendors.length;
+    var div = document.createElement('div'),
+        vendors = 'Khtml Ms O Moz Webkit'.split(' '),
+        len = vendors.length-1;
 
-   return function(prop) {
-      if ( prop in div.style ) return true;
+    return function(prop) {
+        if ( prop in div.style ) return true;
 
-      prop = prop.replace(/^[a-z]/, function(val) {
-         return val.toUpperCase();
-      });
-
-      while(len--) {
-         if ( vendors[len] + prop in div.style ) {
-            // browser supports box-shadow. Do what you need.
-            // Or use a bang (!) to test if the browser doesn't.
-            return true;
-         } 
-      }
-      return false;
-   };
+        prop = prop.replace(/^[a-z]/, function(val) {
+            return val.toUpperCase();
+        });
+        len = vendors.length-1;
+        while(len>=0) {
+            if ( vendors[len] + prop in div.style ) {
+                // browser supports box-shadow. Do what you need.
+                // Or use a bang (!) to test if the browser doesn't.
+                return true;
+            }
+            len--;
+        }
+        return false;
+    };
 })();
